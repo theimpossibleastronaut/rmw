393eb0ca84a5ed83e0729b8d43b30c268188271e
andy5995 <andy400-dev@yahoo.com>
2016-09-18 01:26:22 -0500

    adding 2 files forgotten in previous commit

---
+++ src/functions/messages_rmw.h
@@ -0,0 +1,40 @@
+/*
+ * messages_rmw.h
+ *
+ * This file is part of rmw (https://github.com/andy5995/rmw/wiki)
+ *
+ *  Copyright (C) 2012-2016  Andy Alt (andy400-dev@yahoo.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+#ifndef _INC_MESSAGES_H
+#define _INC_MESSAGES_H
+
+#include "rmw.h"
+#include "strings_rmw.h"
+
+#define MAX_MSG_SIZE 512
+
+void
+open_err (const char *filename, const char *function_name);
+
+short
+close_file (FILE *file_ptr, const char *filename, const char *function_name);
+
+#endif
