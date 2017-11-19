2555292bcb2dda9c18c69869c22fafe385c77f83
andy5995 <andy400-dev@yahoo.com>
2017-11-19 13:26:55 -0600

    [wip] (#107)

--- src/functions/trivial_rmw.c
+++ src/functions/trivial_rmw.c
@@ -46,13 +46,13 @@ Restore FILE(s) from a WASTE directory\n\
   -v, --verbose             increase output messages\n\
   -w, --warranty            display warranty\n\
   -V, --version             display version and license information\n"));
-  printf ("\
+  printf (_("\
   \n\n\
   \t===] Restoring [===\n\n\
   -z, --restore <wildcard filename(s) pattern>\n\
   -s, --select              select files from list to restore\n\
-  -u, --undo-last           undo last ReMove\n");
-  printf ("\
+  -u, --undo-last           undo last ReMove\n"));
+  printf (_("\
   \n\n\
   t===] First time use [===\n\n\
 After rmw is installed, create the user configuration directory\n\
@@ -64,12 +64,12 @@ and then rename it to 'config':\n\n\
   '~/.config/rmw$ mv rmwrc config'\n\n\
 Then edit the file to suit your needs.\n\n\
 Visit the rmw home page for more help, and information about\n\
-how to obtain support - http://github.com/andy5995/rmw/wiki\n\n");
+how to obtain support - http://github.com/andy5995/rmw/wiki\n\n"));
 }
 
 void warranty (void)
 {
-printf ("\
+printf (_("\
 THERE IS NO WARRANTY FOR THE PROGRAM, TO THE EXTENT PERMITTED BY\n\
 APPLICABLE LAW.  EXCEPT WHEN OTHERWISE STATED IN WRITING THE COPYRIGHT\n\
 HOLDERS AND/OR OTHER PARTIES PROVIDE THE PROGRAM \"AS IS\" WITHOUT WARRANTY\n\
@@ -77,20 +77,20 @@ OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO,\n\
 THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR\n\
 PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE PROGRAM\n\
 IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF\n\
-ALL NECESSARY SERVICING, REPAIR OR CORRECTION.\n");
+ALL NECESSARY SERVICING, REPAIR OR CORRECTION.\n"));
 
   exit (0);
 }
 
 void version (void)
 {
-  printf ("\
+  printf (_("\
 rmw %s\n\
 Author: Andy Alt (andy400-dev@yahoo.com)\n\
 The RMW team: see AUTHORS file\n\
 This program comes with ABSOLUTELY NO WARRANTY; for details type 'rmw -w.'\n\
 This is free software, and you are welcome to redistribute it\n\
 under certain conditions; see <http://www.gnu.org/licenses/gpl.html>\n\
-for details.\n", VERSION);
+for details.\n"), VERSION);
   exit (0);
 }
