f52ecc801c4b58b4755237b1c6601b5afa9219d0
Andrew Goldman <atg1119@westminstercollege.edu>
2018-01-17 15:05:00 -0700

    [fixes #137] new messages for purging (#165)
    * [fixes #137] new messages for purging

--- src/purging_rmw.c
+++ src/purging_rmw.c
@@ -160,6 +160,11 @@ purge (const short purge_after, const struct waste_containers *waste,
 
   if (getenv ("RMWTRASH") != NULL)
     cmd_empty = strcmp (getenv ("RMWTRASH"), "empty") ? 0 : 1;
+    
+  if(cmd_empty)
+    printf (_("\nPurging all files in waste folders ...\n"));
+  else
+    printf (_("\nPurging files based on number of days in the waste folders (%u) ...\n"), purge_after);
 
   struct stat st;
 
@@ -173,8 +178,6 @@ purge (const short purge_after, const struct waste_containers *waste,
 
   strptime (time_now, "%Y-%m-%dT%H:%M:%S", &tmPtr);
   now = mktime (&tmPtr);
-
-  printf (_("\nPurging files (purge_after = %u) ...\n"), purge_after);
 
   /**
    *  Read each <WASTE>/info directory
