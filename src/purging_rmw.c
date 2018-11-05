theimpossibleastronaut/rmw

commit 41367fcd65137e4515c559e98d7c53b1cc4412b5

broaden scope of time-related strings (use extern)

src/purging_rmw.c
@@ -157,7 +157,7 @@ static int rmdir_recursive (char *path, short unsigned level, const ushort force
 
 int
 purge (const short purge_after, const st_waste *waste_curr,
-       char *time_now, const ushort force, const char *HOMEDIR)
+  const ushort force, const char *HOMEDIR)
 {
   short status = 0;
 
@@ -181,6 +181,7 @@ purge (const short purge_after, const st_waste *waste_curr,
   time_t now;
   time_t then = 0;
 
+  extern char time_now[];
   strptime (time_now, "%Y-%m-%dT%H:%M:%S", &tmPtr);
   now = mktime (&tmPtr);
 
@@ -393,8 +394,7 @@ purge (const short purge_after, const st_waste *waste_curr,
 }
 
 short
-orphan_maint (st_waste *waste_curr,
-              char *time_now, char *time_str_appended)
+orphan_maint (st_waste *waste_curr)
 {
   rmw_target *file = (rmw_target*)malloc (sizeof (rmw_target));
   chk_malloc (file, __func__, __LINE__);
@@ -433,7 +433,7 @@ orphan_maint (st_waste *waste_curr,
       sprintf (file->real_path, "%s%s%s", waste_curr->parent, "/orphans/", file->base_name);
 
       short ok = 0;
-      ok = create_trashinfo (file, waste_curr, time_now, time_str_appended);
+      ok = create_trashinfo (file, waste_curr);
       if (ok == 0)
       /* TRANSLATORS:  "created" refers to a file  */
         printf (_("Created %s\n"), path_to_trashinfo);

src/rmw.c
@@ -266,7 +266,7 @@ Please check your configuration file and permissions\n\n"));
   /* time_now is used for DeletionDate in trashinfo file
     * and for comparison in purge() */
   /* The length of the format above doesn't exceed 21 */
-  char time_now[21];
+  extern char time_now[];
   get_time_string (time_now, 21, t_fmt);
 
   /** This if statement spits out a message if someone tries to use -g on
@@ -281,26 +281,23 @@ Please check your configuration file and permissions\n\n"));
     if (is_time_to_purge (HOMEDIR) == IS_NEW_DAY || cmd_opt_purge)
     {
       if (force)
-        purge (purge_after, waste_curr, time_now, force, HOMEDIR);
+        purge (purge_after, waste_curr, force, HOMEDIR);
       else if (!created_data_dir)
         printf (_("purge has been skipped: use -f or --force\n"));
     }
   }
 
   /* String appended to duplicate filenames */
-  char time_str_appended[16];
+  extern char time_str_appended[];
   get_time_string (time_str_appended, 16, "_%H%M%S-%y%m%d");
 
   if (orphan_chk)
   {
     waste_curr = waste_head;
-    orphan_maint(waste_curr, time_now, time_str_appended);
+    orphan_maint(waste_curr);
     return 0;
   }
 
-  /* FIXME:
-   * The program can just exit after this.
-   */
   if (select)
   {
     waste_curr = waste_head;
@@ -433,8 +430,7 @@ printf ("file->real_path = %s in %s line %d\n", file->real_path, __func__, __LIN
 DEBUG_PREFIX
 printf ("file->base_name = %s in %s line %d\n", file->base_name, __func__, __LINE__);
 #endif
-            info_status = create_trashinfo (file, waste_curr,
-                              time_now, time_str_appended);
+            info_status = create_trashinfo (file, waste_curr);
 
 #ifdef DEBUG
 DEBUG_PREFIX
@@ -518,8 +514,8 @@ get_time_string (char *tm_str, ushort len, const char *format)
   time_t now = time (NULL);
   time_ptr = localtime (&now);
   strftime (tm_str, len, format, time_ptr);
-  bufchk (tm_str, len);
   trim (tm_str);
+  bufchk (tm_str, len + 1);
 }
 
 /*
@@ -708,3 +704,6 @@ dispose_removed (st_removed *node)
 
 bool list;
 const char *alt_config = NULL;
+char time_now[21];
+char time_str_appended[16];
+

src/trashinfo_rmw.c
@@ -125,8 +125,7 @@ printf ("dest = %s in %s\n", dest, __func__);
 }
 
 int
-create_trashinfo (rmw_target *file, st_waste *waste_curr,
-                  char *time_now, char *time_str_appended)
+create_trashinfo (rmw_target *file, st_waste *waste_curr)
 {
   static char finalInfoDest[MP];
 
@@ -141,6 +140,7 @@ printf ("file->base_name = %s in %s line %d\n", file->base_name, __func__, __LIN
 
   if (file->is_duplicate)
   {
+    extern char time_str_appended[];
     bufchk (time_str_appended, MP - strlen (finalInfoDest));
     strcat (finalInfoDest, time_str_appended);
   }
@@ -164,6 +164,7 @@ printf ("file->base_name = %s in %s line %d\n", file->base_name, __func__, __LIN
       return 1;
     }
 
+    extern char *time_now;
 #ifdef DEBUG
 DEBUG_PREFIX
 printf ("[Trash Info]\n");

src/purging_rmw.h
@@ -35,8 +35,7 @@
 
 int
 purge (const short purge_after, const st_waste *waste_curr,
-        char *time_now, const ushort force, const char *HOMEDIR);
+  const ushort force, const char *HOMEDIR);
 
 short
-orphan_maint(st_waste *waste_curr,
-              char *time_now, char *time_str_appended);
+orphan_maint(st_waste *waste_curr);

src/trashinfo_rmw.h
@@ -27,5 +27,4 @@
 #include <sys/stat.h>
 
 int
-create_trashinfo (rmw_target *file, st_waste *waste_curr,
-                  char *time_now, char *time_str_appended);
+create_trashinfo (rmw_target *file, st_waste *waste_curr);

commit 673e7c848980f8f3636e3ea0ac025580be8f0ef9

bugfix:
segfault when ReMoving multiple files introduced in
https://github.com/theimpossibleastronaut/rmw/commit/41367fcd65137e4515c559e98d7c53b1cc4412b5

src/purging_rmw.c
@@ -181,7 +181,7 @@ purge (const short purge_after, const st_waste *waste_curr,
   time_t now;
   time_t then = 0;
 
-  extern char time_now[];
+  extern char *time_now;
   strptime (time_now, "%Y-%m-%dT%H:%M:%S", &tmPtr);
   now = mktime (&tmPtr);
 

src/rmw.c
@@ -38,6 +38,9 @@
 #include "strings_rmw.h"
 #include "messages_rmw.h"
 
+#define LEN_TIME_NOW 21
+#define LEN_TIME_STR_APPENDED 16
+
 static ushort
 is_time_to_purge (const char *HOMEDIR);
 
@@ -266,7 +269,8 @@ Please check your configuration file and permissions\n\n"));
   /* time_now is used for DeletionDate in trashinfo file
     * and for comparison in purge() */
   /* The length of the format above doesn't exceed 21 */
-  extern char time_now[];
+  extern char *time_now;
+  time_now = calloc (LEN_TIME_NOW, 1);
   get_time_string (time_now, 21, t_fmt);
 
   /** This if statement spits out a message if someone tries to use -g on
@@ -288,7 +292,8 @@ Please check your configuration file and permissions\n\n"));
   }
 
   /* String appended to duplicate filenames */
-  extern char time_str_appended[];
+  extern char *time_str_appended;
+  time_str_appended = calloc (LEN_TIME_STR_APPENDED, 1);
   get_time_string (time_str_appended, 16, "_%H%M%S-%y%m%d");
 
   if (orphan_chk)
@@ -461,6 +466,8 @@ Enter '%s -h' for more information\n"), argv[0]);
   dispose_waste (waste_curr);
   waste_head = NULL;
   free (waste_head);
+  free (time_now);
+  free (time_str_appended);
 
   return 0;
 }
@@ -473,7 +480,7 @@ get_time_string (char *tm_str, ushort len, const char *format)
   time_ptr = localtime (&now);
   strftime (tm_str, len, format, time_ptr);
   trim (tm_str);
-  bufchk (tm_str, len + 1);
+  bufchk (tm_str, len);
 }
 
 /*
@@ -662,6 +669,6 @@ dispose_removed (st_removed *node)
 
 bool list;
 const char *alt_config = NULL;
-char time_now[21];
-char time_str_appended[16];
+char *time_now;
+char *time_str_appended;
 

src/trashinfo_rmw.c
@@ -140,7 +140,7 @@ printf ("file->base_name = %s in %s line %d\n", file->base_name, __func__, __LIN
 
   if (file->is_duplicate)
   {
-    extern char time_str_appended[];
+    extern char *time_str_appended;
     bufchk (time_str_appended, MP - strlen (finalInfoDest));
     strcat (finalInfoDest, time_str_appended);
   }