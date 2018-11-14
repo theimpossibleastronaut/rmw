theimpossibleastronaut/rmw

commit daa05a0d0ac324c6e9e888a5d0dd3faba561c4e1

rmw.c:fix dead condition (DATADIR)

src/rmw.c
@@ -422,19 +422,11 @@ verbose = 1;
   chk_malloc (data_dir, __func__, __LINE__);
   bufchk (HOMEDIR, MP - strlen (DATA_DIR));
   snprintf (data_dir, MP, "%s%s", HOMEDIR, DATA_DIR);
-
-  ushort created_data_dir;
-  created_data_dir = make_dir (data_dir);
+  int created_data_dir = make_dir (data_dir);
   free (data_dir);
   data_dir = NULL;
 
-  /* make_dir now returns MAKE_DIR_SUCCESS or FAILURE
-   * We want to assign something more specific that can be used later
-   */
-  if (created_data_dir)
-    created_data_dir = DATA_DIR_CREATED;
-
-  if (!created_data_dir || created_data_dir == DATA_DIR_CREATED)
+  if (created_data_dir == MAKE_DIR_SUCCESS)
   {}
   else
   {

src/messages_rmw.c
@@ -32,7 +32,6 @@ static const char *ERR_STRING[] = {
   "NO_WASTE_FOLDER",
   "EXIT_BUF_ERR",
   "EXIT_MALLOC_ERR",
-  "DATA_DIR_CREATED",
   "MAKE_DIR_SUCCESS",
   "MAKE_DIR_FAILURE",
   "FORCE_NOT_USED",

src/rmw.h
@@ -120,7 +120,6 @@ enum {
   NO_WASTE_FOLDER,
   EXIT_BUF_ERR,
   EXIT_MALLOC_ERR,
-  DATA_DIR_CREATED,
   MAKE_DIR_SUCCESS,
   MAKE_DIR_FAILURE,
   FORCE_NOT_USED,

src/utils_rmw.c
@@ -41,7 +41,7 @@ int
 make_dir (const char *dir)
 {
   if (exists (dir))
-    return 0;
+    return MAKE_DIR_SUCCESS;
 
   char temp_dir[MP];
   strcpy (temp_dir, dir);

commit 698efaa846ebd92be697148728c658d1116c2ec2

rmw.c:restore output when no args are given
Bug I introduced in
https://github.com/theimpossibleastronaut/rmw/commit/daa05a0d0ac324c6e9e888a5d0dd3faba561c4e1#diff-903b11d1ac3bee9746b853644e245829

src/rmw.c
@@ -668,7 +668,7 @@ Please check your configuration file and permissions\n\n"));
     if (main_error > 1)
       return main_error;
   }
-  else if (!cmd_opt_purge && !created_data_dir)
+  else if (!cmd_opt_purge && created_data_dir == MAKE_DIR_SUCCESS)
       printf (_("No filenames or command line options were given\n\
 Enter '%s -h' for more information\n"), argv[0]);
 