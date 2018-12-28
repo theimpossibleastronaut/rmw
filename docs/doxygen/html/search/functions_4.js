71398c206857c8df3067dd813ccd659c758355cc
andy5995 <andy400-dev@yahoo.com>
2018-12-28 13:40:35 -0600

    regenerate docs

--- docs/doxygen/html/search/functions_4.js
+++ docs/doxygen/html/search/functions_4.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['get_5fconfig_5fdata',['get_config_data',['../config__rmw_8c.html#ae95290d81968748dee591de2939c79b7',1,'get_config_data(void):&#160;config_rmw.c'],['../config__rmw_8h.html#ae95290d81968748dee591de2939c79b7',1,'get_config_data(void):&#160;config_rmw.c']]],
+  ['get_5fthen_5ftime',['get_then_time',['../purging__rmw_8c.html#a0b77956b6dd10e0bb4e7d3432c0ab5bc',1,'purging_rmw.c']]],
   ['get_5ftime_5fstring',['get_time_string',['../rmw_8c.html#a09c314cd2ed48f9465681bbd70c0bd98',1,'rmw.c']]]
 ];
