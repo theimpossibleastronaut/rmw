19ba642c3d6c1962df62f066dcb5218b1b8a26c2
andy5995 <andy400-dev@yahoo.com>
2019-01-26 13:57:51 -0600

    .travis.yml:add key, remove "allow-unauthenticated" option

--- .travis-before_install.sh
+++ .travis-before_install.sh
@@ -6,10 +6,6 @@
 # Copyright (c) 2018
 # modified for rmw <https://github.com/theimpossibleastronaut/rmw/wiki> by Andy Alt
 
-# This only needs to be changed when Travis updates the Ubuntu version
-# it's using.
-codename=trusty
-
 Compiler_name="$1"; Compiler_version="$2"
 Compiler_version_grep="$(echo "$Compiler_version" | sed 's/\./\\./g')"
 
@@ -28,7 +24,8 @@ if [ "$Compiler_version" != "" ] && [ "$Compiler_version" != "default" ]; then
   set -x
   if [ "$VersionAvByDefault" = "" ]; then
     # ubuntu test toolchain needed for more recent version of gcc and clang-6.0
-    sudo add-apt-repository --yes "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu ${codename} main"
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157 &&
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
   fi
 fi
 
@@ -45,16 +42,16 @@ if [ "$Compiler_version" != "" ] && [ "$Compiler_version" != "default" ]; then
     Gcc_AvSepGpp="$(apt-cache search ^g[c+][c+]-[0-9] | grep -v '[0-9]-[a-zA-Z]' | grep "^g++-$Compiler_version_grep")"
     set -ex
     if [ "$Gcc_AvSepGpp" = "" ]; then
-      sudo apt-get --allow-unauthenticated install -qq --force-yes gcc-${Compiler_version}
+      sudo apt-get install -qq -y gcc-${Compiler_version}
     else
-      sudo apt-get --allow-unauthenticated install -qq --force-yes gcc-${Compiler_version} g++-${Compiler_version}
+      sudo apt-get install -qq -y gcc-${Compiler_version} g++-${Compiler_version}
     fi
   elif [ "$Compiler_name" = "clang" ]; then
     # llvm-toolchain needed for more recent version of clang
     wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-    sudo -E apt-add-repository "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main"
+    sudo -E apt-add-repository -y "deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main"
     sudo -E apt-get -yq update &>> ~/apt-get-update.log
-    sudo -E apt-get -yq --no-install-suggests --allow-unauthenticated \
+    sudo -E apt-get -yq --no-install-suggests \
         --no-install-recommends $TRAVIS_APT_OPTS install clang-${Compiler_version}
   fi
 fi
