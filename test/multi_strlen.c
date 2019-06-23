2cf0b764811357dd8a9abb8570c66ec08a7d9eab
andy5995 <andy400-dev@yahoo.com>
2019-06-23 18:48:37 -0500

    test/multi_strlen.c:add unit test for new function

---
+++ test/multi_strlen.c
@@ -0,0 +1,13 @@
+#include "rmw.h"
+#include "strings_rmw.h"
+#include <assert.h>
+
+/* multi_strlen should return a pre-determined length */
+
+int
+main ()
+{
+  char test_string[] = "this is a test string";
+  int start_len = strlen (test_string);
+  assert (multi_strlen (4, "this"," is"," a," "test string") == start_len);
+}
