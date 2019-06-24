15e4e665f401940b16d358fd880738f1eb78510c
andy5995 <andy400-dev@yahoo.com>
2019-06-23 19:00:03 -0500

    test/multi-strlen.c:make sure it doesn't match a bad value

--- test/multi_strlen.c
+++ test/multi_strlen.c
@@ -10,4 +10,6 @@ main ()
   char test_string[] = "this is a test string";
   int start_len = strlen (test_string);
   assert (multi_strlen (4, "this"," is"," a," "test string") == start_len);
+
+  assert (multi_strlen (4, "this"," is"," a," "test string") != 1234);
 }
