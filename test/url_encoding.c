a4b426abc3ec022258b4e55bf6d482f5af0470b8
andy5995 <andy400-dev@yahoo.com>
2019-06-30 16:12:29 -0500

    add unit test for encoding/decoding path names

---
+++ test/url_encoding.c
@@ -0,0 +1,23 @@
+#include "rmw.h"
+#include "trashinfo_rmw.h"
+#include "restore_rmw.h"
+#include <assert.h>
+
+#define BUF_SIZE 80
+
+int
+main ()
+{
+  char str[BUF_SIZE * 3];
+  char escaped_path[BUF_SIZE * 3];
+  strcpy (str, "string to encode \n\t\v  \f\r");
+  escape_url (str, escaped_path, BUF_SIZE * 3);
+  printf ("'%s'\n", escaped_path);
+  assert (!strcmp (escaped_path, "string%20to%20encode%20%0A%09%0B%20%20%0C%0D"));
+
+  char unescaped_path[BUF_SIZE];
+  unescape_url (escaped_path, unescaped_path, BUF_SIZE);
+  assert (!strcmp (unescaped_path, "string to encode \n\t\v  \f\r"));
+  return 0;
+}
+
