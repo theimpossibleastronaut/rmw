4e1d2ff6883c5786e54409184edda5999cf323d5
James Sherratt <jdsherratt3@gmail.com>
2019-07-06 14:19:29 +0100

    Moved comment to correct position.

--- src/rmw.h
+++ src/rmw.h
@@ -105,9 +105,9 @@ struct rmw_options
   bool want_selection_menu;
   bool want_undo;
   ushort force;
-  /*! Alternate configuration file given at the command line with -c */
   /*! list waste folder option */
   bool list;
+  /*! Alternate configuration file given at the command line with -c */
   const char *alt_config;
 };
 
