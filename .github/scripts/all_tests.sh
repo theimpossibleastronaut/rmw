7f8afd8df3746dd6dc8c6c5349a5b90db17d9ed5
andy5995 <andy400-dev@yahoo.com>
2021-07-13 13:07:45 -0500

    move stuff to script
    Signed-off-by: andy5995 <andy400-dev@yahoo.com>

--- .github/scripts/all_tests.sh
+++ .github/scripts/all_tests.sh
@@ -2,13 +2,29 @@
 
 set -ev
 
+sudo apt-get install -y gettext
+
+if [ "$MATRIX_OS" = "ubuntu-latest" && "$MATRIX_ARCH" = "x86" ]; then
+  VALGRIND=1
+  sudo apt-get install -y valgrind
+fi
+
+fi
+
+if [ "$MATRIX_OS" = "ubuntu-18.04" ]; then
+  sudo apt-get install -y python3-pip
+fi
+
+sudo python3 -m pip install meson ninja
+
 meson builddir
 cd builddir
 ninja -v
 ninja dist
 
-# valgrind
-meson test --setup=valgrind
+if [ -v VALGRIND ]; then
+  meson test --setup=valgrind
+fi
 
 # fake media root
 meson test --setup=fake_media_root
