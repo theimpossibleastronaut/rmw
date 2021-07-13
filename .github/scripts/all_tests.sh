96450f756b0ecaea9a5ad9dc97f6aae8f5a485a3
andy5995 <andy400-dev@yahoo.com>
2021-07-13 13:47:24 -0500

    add build matrix to workflow

--- .github/scripts/all_tests.sh
+++ .github/scripts/all_tests.sh
@@ -2,13 +2,29 @@
 
 set -ev
 
+sudo apt-get install -y gettext
+
+USE_VALGRIND=""
+
+if [ "$MATRIX_OS" = "ubuntu-latest" ] && [ "$MATRIX_ARCH" = "x86" ]; then
+  USE_VALGRIND=1
+  sudo apt-get install -y valgrind
+fi
+
+if [ "$MATRIX_OS" = "ubuntu-18.04" ]; then
+  sudo apt-get install -y python3-pip python3-setuptools
+fi
+
+sudo -H python3 -m pip install meson ninja
+
 meson builddir
 cd builddir
 ninja -v
 ninja dist
 
-# valgrind
-meson test --setup=valgrind
+if [ -n USE_VALGRIND ]; then
+  meson test --setup=valgrind
+fi
 
 # fake media root
 meson test --setup=fake_media_root
