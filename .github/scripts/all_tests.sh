e10901d46a41d96489287f3863153b54685b5efc
andy5995 <arch_stanton5995@protonmail.com>
2022-09-20 13:36:51 -0500

    Add appimage build when releasing

--- .github/scripts/all_tests.sh
+++ .github/scripts/all_tests.sh
@@ -2,6 +2,8 @@
 
 set -ev
 
+BUILDDIR="$PWD/builddir"
+
 sudo apt-get install -y gettext
 
 USE_VALGRIND=""
@@ -21,26 +23,45 @@ else
    sudo -H python3 -m pip install meson ninja
 fi
 
-meson builddir
-cd builddir
-ninja -v
-ninja dist
+meson $BUILDDIR
+cd $BUILDDIR
 
-if [ -n "$USE_VALGRIND" ]; then
-  meson test --setup=valgrind
-fi
-
-# fake media root
-meson test --setup=fake_media_root --suite rmw
-
-# nls disabled
-meson configure -Dnls=false
-ninja -v
-meson test -v
-
-# curses disabled
-meson configure -Dwithout-curses=true
-if [ -n "$USE_VALGRIND" ]; then
+if [ "${GITHUB_REF_TYPE}" != "tag" ]; then
   ninja -v
-  meson test --setup=valgrind
+  ninja dist
+
+  if [ -n "$USE_VALGRIND" ]; then
+    meson test --setup=valgrind
+  fi
+
+  # fake media root
+  meson test --setup=fake_media_root --suite rmw
+
+  # nls disabled
+  meson configure -Dnls=false
+  ninja -v
+  meson test -v
+
+  # curses disabled
+  meson configure -Dwithout-curses=true
+  if [ -n "$USE_VALGRIND" ]; then
+    ninja -v
+    meson test --setup=valgrind
+  fi
+else
+  VERSION=${GITHUB_REF_NAME:1}
+  meson configure -Dbuildtype=release -Dstrip=true -Dprefix=/usr
+  DESTDIR=AppDir ninja install
+  cp /usr/bin/rm AppDir/usr/bin
+  cd -
+  wget -nv https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-${ARCH}.AppImage
+  chmod +x linuxdeploy-${ARCH}.AppImage
+  ./linuxdeploy-${ARCH}.AppImage  \
+    -d packaging/rmw.desktop \
+    --icon-file=packaging/rmw_icon_32x32.png \
+    --icon-filename=rmw \
+    --executable ${BUILDDIR}/AppDir/usr/bin/rmw \
+    --appdir ${BUILDDIR}/AppDir \
+    --output appimage
+  sha256sum rmw-${VERSION}-${ARCH}.AppImage > rmw-${VERSION}-${ARCH}.AppImage.sha256sum
 fi
