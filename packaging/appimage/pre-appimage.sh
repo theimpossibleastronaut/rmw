4b02d6db1ff856d68a9f0843d894c10cd83f8911
andy5995 <arch_stanton5995@protonmail.com>
2024-02-12 03:54:30 -0600

    Update pre-appimage script

--- packaging/appimage/pre-appimage.sh
+++ packaging/appimage/pre-appimage.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
 
+# Set bash options:
+# -e: Exit immediately if a command exits with a non-zero status.
+# -v: Print each command to stderr before executing it.
 set -ev
 
-WORKSPACE=${WORKSPACE:-$ACTION_WORKSPACE}
-if [ -z "$WORKSPACE" ]; then
-  echo "You need to set the WORKSPACE directory."
-  exit 1
-fi
+# Set default workspace if not provided
+WORKSPACE=${WORKSPACE:-$(pwd)}
+# Check if the workspace path is absolute
 if [[ "$WORKSPACE" != /* ]]; then
   echo "The workspace path must be absolute"
   exit 1
 fi
 test -d "$WORKSPACE"
 
-SOURCE_ROOT=${SOURCE_ROOT:-$ACTION_SOURCE_ROOT}
-if [ -z "$SOURCE_ROOT" ]; then
-  echo "You need to set the SOURCE_ROOT directory."
-  exit 1
-fi
+# Set default source root if not provided
+SOURCE_ROOT=${SOURCE_ROOT:-$WORKSPACE}
+# Check if the source root path is absolute
 if [[ "$SOURCE_ROOT" != /* ]]; then
   echo "The source root path must be absolute"
   exit 1
 fi
-test -d "$SOURCE_ROOT"
+# Verify that you're in the source root
+echo $SOURCE_ROOT
+test -f "$SOURCE_ROOT/src/main.c"
 
+# Define and create application directory if it doesn't exist
+# This is the directory where your project will be installed to
+# and an AppImage created
+APPDIR=${APPDIR:-"/tmp/$USER-AppDir"}
+if [ -d "$APPDIR" ]; then
+  rm -rf "$APPDIR"
+else
+  mkdir -v -p "$APPDIR"
+fi
+
+# Install necessary dependencies
 sudo apt install -y \
   gettext \
   liblua5.1-0-dev \
@@ -32,25 +44,43 @@ sudo apt install -y \
   libsdl2-mixer-dev \
   libsdl2-ttf-dev
 
-export APPDIR=${APPDIR:-"$WORKSPACE/AppDir"}
-
-if [ -d "$APPDIR" ]; then
-  rm -rf "$APPDIR"
-fi
-
+# Set up build directory
 BUILD_DIR="$SOURCE_ROOT/appimage_build"
-
-if [ -d "$APPDIR" ] && [ "$CLEAN_BUILD" = "true" ]; then
-  rm -rf "$APPDIR"
+cd "$SOURCE_ROOT"
+BUILD_DIR="$SOURCE_ROOT/_build_prep_appdir"
+# Clean build directory if specified and it exists
+if [ "$CLEAN_BUILD" = "true" ] && [ -d "$BUILD_DIR" ]; then
+  rm -rf "$BUILD_DIR"
 fi
 
-cd "$SOURCE_ROOT"
-meson setup $BUILD_DIR \
-  -Dbuildtype=release \
-  -Dstrip=true \
-  -Db_sanitize=none \
-  -Dprefix=/usr
+# Setup project for building
+if [ ! -d "$BUILD_DIR" ]; then
+  meson setup "$BUILD_DIR" \
+    -Dbuildtype=release \
+    -Dstrip=true \
+    -Db_sanitize=none \
+    -Dprefix=/usr
+fi
 
+# Build project
 cd "$BUILD_DIR"
 ninja
 meson install --destdir=$APPDIR --skip-subprojects
+
+# Set up output directory
+OUT_DIR="$WORKSPACE/out"
+if [ ! -d "$OUT_DIR" ]; then
+  mkdir "$OUT_DIR"
+fi
+cd "$OUT_DIR"
+
+# Set LinuxDeploy output version
+LINUXDEPLOY_OUTPUT_VERSION="$VERSION"
+# Generate AppImage using linuxdeploy
+linuxdeploy \
+  --appdir="$APPDIR" \
+  -d $SOURCE_ROOT/packaging/rmw.desktop \
+  --icon-file=$SOURCE_ROOT/packaging/rmw_icon_32x32.png \
+  --icon-filename=rmw \
+  --executable=$APPDIR/usr/bin/rmw \
+  -o appimage
