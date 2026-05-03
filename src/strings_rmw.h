b397af5eb8cd945887606502f8b466abb2cfa404
andy5995 <arch_stanton5995@proton.me>
2026-05-03 11:13:27 -0500

    Fix BSD warnings

--- src/strings_rmw.h
+++ src/strings_rmw.h
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include <ctype.h>
-#include <libgen.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <string.h>
